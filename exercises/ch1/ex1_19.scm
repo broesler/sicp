21caf158bf26e7bf28216d2901970bcc69e99614
Bernard Roesler <bernard.roesler@gmail.com>
2016-11-03 00:49:45 -0400

    Complete ex1_19

---
+++ exercises/ch1/ex1_19.scm
@@ -0,0 +1,31 @@
+;;==============================================================================
+;;     File: ex1_19.scm
+;;  Created: 11/03/2016, 00:29
+;;   Author: Bernie Roesler
+;;
+;;  Description: Fibonacci numbers in log time
+;;
+;;==============================================================================
+
+; initial call
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+; iteration procedure
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (square p) (square q))      ; compute p'
+                   (+ (square q) (* 2 p q))       ; compute q'
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+
+;;==============================================================================
+;;==============================================================================
