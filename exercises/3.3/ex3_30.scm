5c99740aa51ac8dbf10751fd9ecab31fdb76e120
Bernard Roesler <bernard.roesler@gmail.com>
2017-02-01 22:29:38 -0500

    Complete through ex 3.33

--- exercises/3.3/ex3_30.scm
+++ exercises/3.3/ex3_30.scm
@@ -32,5 +32,39 @@
 ;;; NOTE carry bit is the propagating signal. It depends on "B" to propagate
 ;;; through two half-adders (first step), which each require that "B" pass
 ;;; through just one and-gate to produce the carry bit (second step).
+
+;;; Test Code:
+(load "circuit_simulation.scm") ; circuit primitives
+
+(define A1 (make-wire)) ; input-1 (3-bit number)
+(define A2 (make-wire))
+(define A3 (make-wire))
+(define B1 (make-wire)) ; input-2 (3-bit number)
+(define B2 (make-wire))
+(define B3 (make-wire))
+(define S1 (make-wire)) ; sum (3-bit number)
+(define S2 (make-wire))
+(define S3 (make-wire))
+(define C  (make-wire)) ; carry bit
+
+(define A (list A1 A2 A3))
+(define B (list B1 B2 B3))
+(define S (list S1 S2 S3))
+
+(probe 'sum1 S1)
+(probe 'sum2 S2)
+(probe 'sum3 S3)
+(probe 'carry C)
+
+(ripple-carry-adder A B S C)
+
+;; Add 101 + 001 = 110 (i.e. 5 + 1 = 6)
+(set-signal! A1 1)
+(set-signal! A2 0)
+(set-signal! A3 1)
+(set-signal! B1 0)
+(set-signal! B2 0)
+(set-signal! B3 1)
+(propagate)
 ;;==============================================================================
 ;;==============================================================================
